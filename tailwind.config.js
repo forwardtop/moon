0e588b3b0109ada29f0631f2a5ef46d7aa96f0f9
forwardtop <noproblemgod@hotmail.com>
2023-11-21 04:23:34 +0800

    222

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+// tailwind.config.js
+
+module.exports = {
+  content: [
+    './src/**/*.{html,vue,js}', // Include these file types for Tailwind CSS processing
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
